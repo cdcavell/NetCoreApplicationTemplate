97a4e2f6d74a32a17b6f740b08a522fbecc5ab87
Chris Cavell <cdcavell@gmail.com>
2026-05-01 14:53:46 -0500

    Add request logging middleware

--- src/Template.Web/Extensions/PipelineExtensions.cs
+++ src/Template.Web/Extensions/PipelineExtensions.cs
@@ -11,34 +11,12 @@ namespace Template.Web.Extensions
             app.UseForwardedHeaders();
 
             // 2. Structured request logging should see corrected scheme, host, and client IP.
-            app.UseSerilogRequestLogging(options =>
-            {
-                options.MessageTemplate =
-                    "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
-
-                options.GetLevel = (httpContext, elapsed, exception) =>
-                {
-                    if (exception is not null)
-                        return LogEventLevel.Error;
-
-                    return httpContext.Response.StatusCode >= 500
-                        ? LogEventLevel.Error
-                        : LogEventLevel.Information;
-                };
-
-                options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
-                {
-                    diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
-                    diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
-                    diagnosticContext.Set("RemoteIpAddress", httpContext.Connection.RemoteIpAddress?.ToString());
-                    diagnosticContext.Set("UserAgent", httpContext.Request.Headers.UserAgent.ToString());
-                };
-            });
+            app.UseTemplateRequestLogging();
 
             // 3. Centralized exception handling.
+            app.UseExceptionHandler("/error/500");
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/error/500");
                 app.UseHsts();
             }
 
@@ -58,7 +36,7 @@ namespace Template.Web.Extensions
             //app.UseCors();
 
             // 9. Rate limiting after routing when endpoint-specific policies are used.
-            //app.UseRateLimiter();
+            app.UseRateLimiter();
 
             // 10. Authentication and authorization.
             //app.UseAuthentication();

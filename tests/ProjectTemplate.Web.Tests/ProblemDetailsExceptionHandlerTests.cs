5237412859b79796d29713704ead9ecd461f3715
Chris Cavell <cdcavell@gmail.com>
2026-05-30 21:18:46 -0500

    test: add Problem Details exception handler contract coverage

--- tests/ProjectTemplate.Web.Tests/ProblemDetailsExceptionHandlerTests.cs
+++ tests/ProjectTemplate.Web.Tests/ProblemDetailsExceptionHandlerTests.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging.Abstractions;
 using ProjectTemplate.Web.ErrorHandling;
-using Microsoft.AspNetCore.Http.Features;
 
 namespace ProjectTemplate.Web.Tests;
 
